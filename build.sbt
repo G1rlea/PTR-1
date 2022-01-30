32eb4b3ff3e5a3e3acf2b193f37bd77976ec89a3
Adrian Girlea <girlea.adrian@isa.utm.md>
2022-01-30 16:09:44 +0200

    Add non-permanent sse connection

--- build.sbt
+++ build.sbt
@@ -6,6 +6,16 @@ lazy val root = (project in file("."))
   .settings(
     name := "PTR-1"
   )
-libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.18"
-libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
-libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.10"
+
+val AkkaVersion = "2.6.18"
+val AkkaHttpVersion = "10.2.7"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4",
+  "ch.qos.logback" % "logback-classic" % "1.2.10",
+  "net.liftweb" %% "lift-json" % "3.5.0",
+  "de.heikoseeberger" %% "akka-sse" % "3.0.0",
+)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,26 @@
-import akka.actor.{ActorSystem, Props}
-import myactor.{MyActor, MyActorWithParam}
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding.Get
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import de.heikoseeberger.akkasse.scaladsl.model.ServerSentEvent
+import de.heikoseeberger.akkasse.scaladsl.unmarshalling.EventStreamUnmarshalling
+
+case class MailServer(url: String, username: String, password: String)
 
 object Main extends App{
-  val system: ActorSystem = ActorSystem("MySystem")
-  val actor1 = system.actorOf(Props[MyActor](), name = "myactor1")
-  val actor2 = system.actorOf(Props(new MyActorWithParam(actor1)), name = "actor2")
-  actor2 ! "start"
+  implicit val system = ActorSystem()
+
+  import EventStreamUnmarshalling._
+  import system.dispatcher
+  Http()
+    .singleRequest(Get("http://localhost:8000/tweets/1"))
+    .flatMap(Unmarshal(_).to[Source[ServerSentEvent, NotUsed]])
+    .foreach(_.runForeach(println))
+
+
 }
