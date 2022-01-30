61696e7e62b479e401df4b066e164b875cc0d3e9
Adrian Girlea <girlea.adrian@isa.utm.md>
2022-01-30 13:04:26 +0200

    Add 2 actors able to communicate and create children

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,9 @@
-import akka.actor.{ActorRef, ActorSystem, Props}
-import com.typesafe.scalalogging.*
+import akka.actor.{ActorSystem, Props}
+import myactor.{MyActor, MyActorWithParam}
 
 object Main extends App{
   val system: ActorSystem = ActorSystem("MySystem")
   val actor1 = system.actorOf(Props[MyActor](), name = "myactor1")
-  val actor2 = system.actorOf(Props[MyActor](), name = "myactor2")
-  println("Main thread 1")
-  actor1 ! "Hi There"
-  actor1 ! 6
-  actor2 ! 6
-  println("Main thread 2")
-  actor1 ! "Hi There"
-  actor1 ! Option[1]
-  println("Main thread 3")
-//  system.terminate()
+  val actor2 = system.actorOf(Props(new MyActorWithParam(actor1)), name = "actor2")
+  actor2 ! "start"
 }
--- src/main/scala/myactor/MyActor.scala
+++ src/main/scala/myactor/MyActor.scala
@@ -1,24 +1,57 @@
-package ;
+package myactor
 
-import akka.actor.{Actor, ActorRef}
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.{Actor, ActorRef, Props, PoisonPill}
+import com.typesafe.scalalogging.LazyLogging
 import org.w3c.dom.TypeInfo
 
-class MyActor extends Actor{
-  case class Tweet(content: String)
+import java.util.UUID
+
+class MyActor extends Actor with LazyLogging{
+
+  var countReceivedMessages = 0;
+
+  def concat: Unit = countReceivedMessages += 1
+  def getName: String = UUID.randomUUID().toString;
+
   def receive: Receive = {
     case s: Tweet =>
-      Thread.sleep(3000)
-      println(Thread.currentThread().getName)
-      println(s"We got a Tweet $s.content")
-    case s: Int =>
-      println(Thread.currentThread().getName)
-      Thread.sleep(2000)
-      println("We got a integer")
+      println(s"We got a tweet from ${self.path}")
+//      sender() ! "adfaf"
+
+    case s: String =>
+      Thread.sleep(1000);
+      concat
+      if (countReceivedMessages > 4){
+        context.system.terminate()
+      }
+      val childActor = context.actorOf(Props[MyActor](), name = getName)
+      val childActor2 = context.actorOf(Props[MyActor](), name = getName)
+      self ! Tweet("some twwt")
+      childActor ! Tweet("Some tweet")
+      childActor2 ! Tweet("Some tweet")
+      logger.info("Actor 1 received the message")
+      println(s"$s")
+      sender() ! s
+
     case _ =>
       println(Thread.currentThread().getName)
       println("I really don't give a shit")
   }
-
 }
+
+class MyActorWithParam(actor: ActorRef) extends Actor with LazyLogging {
+  def receive: Receive = {
+    case "start" =>
+      Thread.sleep(1000);
+      logger.info("Actor 2 send the message")
+      actor ! "message"
+    case message: String =>
+      Thread.sleep(1000);
+      logger.info("Actor 2 received and send the message")
+      sender() ! message
+  }
+}
+
+case class Tweet(content: String)
